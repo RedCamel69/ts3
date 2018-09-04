27b4a15a8c5482a0c25c0fe91ce8bea7afb3ff15
Richard Gilbert <RichardJGilbert@hotmail.com>
2018-09-04 14:48:55 +0100

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { SearchComponent } from '../app/search/search.component'
-import { IndexComponent } from '../app/index/index.component'
+import { SearchComponent } from '../app/search/search.component';
+import { IndexComponent } from '../app/index/index.component';
 
 const routes: Routes = [
 
