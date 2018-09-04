ed601d340f223a9bc60c5d2a7b964d0efadc41ac
Richard Gilbert <RichardJGilbert@hotmail.com>
2018-09-04 14:43:50 +0100

    Add files via upload

--- src/app.component.ts
+++ src/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { SearchService } from '../app/search.service'
+import { SearchService } from './app/search.service'
 
 
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
+    //,styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'ts3';
