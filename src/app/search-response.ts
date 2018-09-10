6b4153004b66ee93e456b013261284bb80c1b692
Richard Gilbert <RichardJGilbert@hotmail.com>
2018-09-10 16:57:09 +0100

    Add files via upload

--- src/app/search-response.ts
+++ src/app/search-response.ts
@@ -1,21 +1,21 @@
 export class SearchResponse {
-  webPages: webPages;
-  queryContext: queryContext;
+    webPages: webPages;
+    queryContext: queryContext;
 }
 
 
 export class webPages {
 
-  totalEstimatedMatches: Number;
-  value: Array<value>;
+    totalEstimatedMatches: Number;
+    value: Array<value>;
 }
 
 export class value {
-  displayUrl: string;
-  name: string;
+    displayUrl: string;
+    name: string;
 }
 
 
 export class queryContext {
-  originalQuery: string;
+    originalQuery: string;
 }
