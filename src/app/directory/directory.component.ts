5402a0b61aa2a29f2d49a24add8196edc8045541
Richard Gilbert <RichardJGilbert@hotmail.com>
2018-11-12 18:48:56 +0000

    Add files via upload

---
+++ src/app/directory/directory.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder, Validators, AbstractControl, ValidationErrors } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-directory',
+  templateUrl: './directory.component.html',
+  styleUrls: ['./directory.component.css']
+})
+export class DirectoryComponent implements OnInit {
+
+    searchForm: FormGroup;
+
+    constructor(private fb: FormBuilder) {
+    }
+
+    ngOnInit() {
+        this.searchForm = this.fb.group({
+            search: [null, [Validators.required]]
+        });
+  }
+
+}
